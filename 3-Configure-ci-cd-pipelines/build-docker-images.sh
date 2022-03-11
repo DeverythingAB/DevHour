07fb464438c2f271931f7c09228629254812caf0
Marthin Bergstrand <marthin.bergstrand@deverything.se>
2022-03-11 15:53:21 +0100

    Added workflow

--- 3-Configure-ci-cd-pipelines/build-docker-images.sh
+++ 3-Configure-ci-cd-pipelines/build-docker-images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build -t order-service ./order-service
-docker build -t product-service ./product-service
-docker build -t report-service ./report-service
+
+
+docker build -t order-service ./order-service --build-arg targetFolder=$1
+docker build -t product-service ./product-service --build-arg targetFolder=$1
+docker build -t report-service ./report-service --build-arg targetFolder=$1
